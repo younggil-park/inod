b4a8e7c6a7cd4a77c0a502cd52773fa79cc2c80d
younggil-park <94460471+younggil-park@users.noreply.github.com>
2022-05-24 13:07:02 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,3 +5,5 @@ date_month=`date |cut -d ' ' -f2`
 date_day=`date |cut -d ' ' -f3`
 full_date="$date_year"_"$date_month"_"$date_day"
 ./runserver.py > /var/log/inod/web_log_"$full_date".txt 2>&1 &
+./inod_send.py > /var/log/inod/send_log_"$full_date".txt 2>&1 &
+./inod_read.py > /var/log/inod/read_log_"$full_date".txt 2>&1 &
